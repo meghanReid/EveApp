9dda52535ecf35b2a372e425fdda82d4406f699f
meghanReid <megreid115@hotmail.com>
2016-05-26 17:52:56 -0700

    System details load. Get better details!

--- app/src/main/java/com/mereid/eveonlinemonitor/dummy/SolarSystemDetailFragment.java
+++ app/src/main/java/com/mereid/eveonlinemonitor/dummy/SolarSystemDetailFragment.java
@@ -1,6 +1,7 @@
 package com.mereid.eveonlinemonitor.dummy;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -62,7 +63,25 @@ public class SolarSystemDetailFragment extends Fragment {
 
         // Show the dummy content as text in a TextView.
         if (mItem != null) {
-            ((TextView) rootView.findViewById(R.id.solarsystem_detail)).setText(mItem.systemID);
+            TextView systemIdView = (TextView) rootView.findViewById(R.id.solarsystem_detail);
+            systemIdView.setTextColor(Color.WHITE);
+            systemIdView.setText(mItem.systemID);
+
+            TextView secView = (TextView) rootView.findViewById(R.id.sec_status);
+            secView.setText(mItem.securityStatus);
+            secView.setTextColor(Color.WHITE);
+
+            TextView shipKillsView = (TextView) rootView.findViewById(R.id.system_kills);
+            shipKillsView.setText(Integer.toString(mItem.shipKills));
+            shipKillsView.setTextColor(Color.WHITE);
+
+            TextView podKillView = (TextView) rootView.findViewById(R.id.pod_kills);
+            podKillView.setText(Integer.toString(mItem.podKills));
+            podKillView.setTextColor(Color.WHITE);
+
+            TextView totalKillsView = (TextView) rootView.findViewById(R.id.total_kills);
+            totalKillsView.setText(Integer.toString(mItem.shipKills+mItem.podKills));
+            totalKillsView.setTextColor(Color.WHITE);
         }
 
         return rootView;
